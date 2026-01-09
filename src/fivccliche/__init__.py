8d901234032b2d3b28b74bcb05f1fa75e7a60346
Charlie Zhang <sunnypig2002@gmail.com>
2026-01-09 15:22:26 +0800

    update deps

--- src/fivccliche/__init__.py
+++ src/fivccliche/__init__.py
@@ -15,7 +15,7 @@ Key Features:
 - Built on FastAPI for high performance
 """
 
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 __author__ = "Charlie Zhang"
 __email__ = "sunnypig2002@gmail.com"
 __license__ = "MIT"
