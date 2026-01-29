5ced828fee1be29331f8c572348d6960cd4d0013
Charlie ZHANG <sunnypig2002@gmail.com>
2026-01-29 14:55:42 +0800

    add additional middleware

--- src/fivccliche/services/implements/__init__.py
+++ src/fivccliche/services/implements/__init__.py
@@ -7,15 +7,18 @@ import os
 from fivcglue import LazyValue, IComponentSite
 from fivcglue.implements.utils import load_component_site
 
+service_yml = os.environ.get("SERVICE_YAML") or os.path.join(
+    os.path.dirname(__file__),
+    os.pardir,
+    os.pardir,
+    "settings",
+    "services.yml",
+)
+service_yml = os.path.abspath(service_yml)
+
 service_site: LazyValue[IComponentSite] = LazyValue(
     lambda: load_component_site(
-        filename=os.path.join(
-            os.path.dirname(__file__),
-            os.pardir,
-            os.pardir,
-            "settings",
-            "services.yml",
-        ),
+        filename=service_yml,
         fmt="yaml",
     )
 )
